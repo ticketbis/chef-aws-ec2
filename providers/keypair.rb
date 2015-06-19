4e5be85f875fb1e1d9f3ee1b681e95ad9b6217ff
Alberto Tablado <alberto@aluki.org>
2015-06-19 14:02:55 +0200

    Load current resource

--- providers/keypair.rb
+++ providers/keypair.rb
@@ -5,6 +5,9 @@ end
 use_inline_resources
 
 def load_current_resource
+  @current_resource = Chef::Resource::AwsEc2Keypair.new @new_resource.name
+  current_resource.client = Chef::AwsEc2.get_client new_resource.access_key_id, new_resource.secret_access_key, new_resource.region
+  current_resource.keypair = Chef::AwsEc2.get_keypair current_resource.name, current_resource.client unless current_resource.client.nil?
 end
 
 action :create do
