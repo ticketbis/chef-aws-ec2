4d6802a094c10b5a808a97a2d33d0d737791ff81
Alberto Tablado <alberto@aluki.org>
2015-06-19 13:33:35 +0200

    Initial resource

---
+++ resources/keypair.rb
@@ -0,0 +1,19 @@
+actions :create, :delete
+
+default_action :create
+
+attribute :name, kind_of: String, name_attribute: true
+attribute :publickey, kind_of: String, required: true
+attribute :region, kind_of: String, required: true
+attribute :access_key_id, kind_of: String, required: true
+attribute :secret_access_key, kind_of: String, required: true
+
+attr_accessor :client, :keypair
+
+def exists?
+  not keypair.nil?
+end
+
+def id
+  keypair.id unless keypair.nil?
+end
