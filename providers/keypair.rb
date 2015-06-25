29d0cd61d3ae4dbbe295f289775344f2282566c9
Alberto Tablado <alberto@aluki.org>
2015-06-25 11:55:31 +0200

    New way of getting Resources

--- providers/keypair.rb
+++ providers/keypair.rb
@@ -7,7 +7,7 @@ end
 use_inline_resources
 
 def load_current_resource
-  @current_resource = Chef::Resource::AwsEc2Keypair.new @new_resource.name
+  self.current_resource = Chef::Resource.resource_for_node(new_resource.declared_type, node).new @new_resource.name
   current_resource.client = Chef::AwsEc2.get_client aws_credentials, aws_region
   current_resource.keypair = Chef::AwsEc2.get_keypair current_resource.name, current_resource.client unless current_resource.client.nil?
 end
--- providers/subnet.rb
+++ providers/subnet.rb
@@ -7,7 +7,7 @@ end
 use_inline_resources
 
 def load_current_resource
-  @current_resource = Chef::Resource::AwsEc2Subnet.new @new_resource.name
+  self.current_resource = Chef::Resource.resource_for_node(new_resource.declared_type, node).new @new_resource.name
   @current_resource.client = Chef::AwsEc2::get_client aws_credentials, aws_region
   @current_resource.vpc_o = Chef::AwsEc2.get_vpc @new_resource.vpc, @current_resource.client
   @current_resource.subnet = Chef::AwsEc2.get_subnet @current_resource.vpc_o, @new_resource.name, @current_resource.client
