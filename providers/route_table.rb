a7d3f42d23313eb421f1f2e300061c4832e94a09
Alberto Tablado <alberto@aluki.org>
2015-06-30 16:28:41 +0200

    Manage routing thru NAT

--- providers/route_table.rb
+++ providers/route_table.rb
@@ -13,8 +13,20 @@ def load_current_resource
   @current_resource.route_table = get_route_table unless @current_resource.vpc_o.nil?
   unless @current_resource.route_table.nil?
     @current_resource.routes_o = @current_resource.route_table.routes.inject({}) do |acc,re|
-      unless re.gateway_id.nil?
+      if !re.gateway_id.nil?
         next acc.update({ re.destination_cidr_block => 'gateway' }) if re.gateway_id.start_with? 'igw-'
+      elsif !re.instance_id.nil?
+        instance = current_resource.client.describe_instances(instance_ids: [re.instance_id]).reservations.first.instances.first
+        image = Chef::AwsEc2.get_image(aws_region, 'nat')
+        if instance.image_id == image
+          subnet = Aws::EC2::Subnet.new(instance.subnet_id, client: current_resource.client)
+          fail "An extrange error has occurred. I cannot find the subnet '#{instance.subnet_id}' in which instance '#{instance.instance_id}' lives" if subnet.nil?
+          name = subnet.tags.find{|t| t.key == 'Name'}.value
+          fail "'#{subnet.id}' subnet's name not found" if name.nil?
+          next acc.update({ re.destination_cidr_block => "nat@#{name}"})
+        else
+          fail "Routing thur a non-NAT machine"
+        end
       end
       acc
     end
@@ -71,10 +83,25 @@ def massage_routes rt
 end
 
 def create_route cidr, dest
-  if dest == 'gateway'
+  case dest
+  when 'gateway'
     ig = @current_resource.vpc_o.internet_gateways.first
     raise "VPC does not have an Internet Gatewat and routing thru it was requested" if ig.nil?
     @current_resource.route_table.create_route destination_cidr_block: cidr, gateway_id: ig.id
+  when /^nat@(.*)/
+   subnet = Chef::AwsEc2.get_subnet(current_resource.vpc_o, $1)
+   fail "Unable to find subnet '#{$1}' in VPC '#{new_resource.vpc}'" if subnet.nil?
+   # get NAT machine in the subnet
+   image_id = Chef::AwsEc2.get_image(aws_region, 'nat')
+   fail "Unable to find image 'nat' in region '#{aws_region}'" if image_id.nil?
+   instance = current_resource.client.describe_instances(
+    filters: [
+      { name: 'subnet-id', values: [subnet.id]},
+      { name: 'image-id', values: [image_id]}
+    ]
+    ).reservations.first.instances.first
+   fail "Unable to find a NAT machine in subnet '#{$1}' in VPC '#{new_resource.vpc}'" if instance.nil?
+   @current_resource.route_table.create_route destination_cidr_block: cidr, instance_id: instance.instance_id
   else
     raise 'TODO: we only support "gateway"'
   end
