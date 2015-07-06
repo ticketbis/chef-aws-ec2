4655773e71d48a4df302c7036241da90dbcb8c2d
Alberto Tablado <alberto@aluki.org>
2015-07-06 19:33:53 +0200

    Doc

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,12 @@
 name 'aws-ec2'
 maintainer 'Alberto Tablado'
 maintainer_email 'alberto.tablado@ticketbis.com'
-license 'all_rights'
-description 'Installs/Configures aws-ec2'
-long_description 'Installs/Configures aws-ec2'
+license 'GPL v3'
+description 'Manage resources in AWS EC2'
+long_description IO.read(File.join(
+  File.dirname(__FILE__), 'README.md'
+  )
+)
 version '0.1.0'
 
 depends 'aws-base'
